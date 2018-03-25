775dccc437798ff12b02d8265c76c05eed719632
dekilgrind <32001529+dekilgrind@users.noreply.github.com>
2018-03-25 11:23:05 +0700

    Update test_pushMessage.js

--- test_pushMessage.js
+++ test_pushMessage.js
@@ -3,9 +3,9 @@ const line = require('./index')
 
 // init with auth token
 line.init({
-  accessToken: '{YOUR_ACCESS_TOKEN}',
+  accessToken: 'BIgjEJ10YTGLTeSgeI17mwDOHjltpqM9eSwa3Vd8OBy1DMKOieslwiEM7hx/G+HUAKPCzzeGpTpyTLkB7AABQ1U5+NsTwK/dp8EingGgIIl7gLglcl0MVYUUf0H4V5zPuqNGdUXHN0gW4G/FJA1kdAdB04t89/1O/w1cDnyilFU=',
   // (Optional) for webhook signature validation
-  channelSecret: '{YOUR_CHANNEL_SECRET}'
+  channelSecret: '68325863c8d4ba7acf0cc2d4699bfc49'
 })
 
 line.client
